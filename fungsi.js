145f7e24c02c859b2ea307c8724155c4116adb72
Febrian <febrian013r@gmail.com>
2024-12-10 17:04:53 +0700

    Mengedit sedikit file

--- fungsi.js
+++ fungsi.js
@@ -5,9 +5,8 @@ const subtract = (a,b) => a-b;
 console.log(subtract(5,3));
 
 
-const multiplyNumberOne = (a,b) => a*b;
-console.log(multiplyNumberOne(2,2));
-
+const multiply = (a,b) => a*b;
+console.log(multiply(2,2));
 
 const divide = (a,b) => a/b;
 console.log(divide(10,2));
