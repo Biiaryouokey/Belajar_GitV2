ed48c9833940432ca77d3ed3704104de906b5db5
Febrian <febrian013r@gmail.com>
2024-12-10 13:45:51 +0700

    revisi multiply

--- fungsi.js
+++ fungsi.js
@@ -5,8 +5,9 @@ const subtract = (a,b) => a-b;
 console.log(subtract(5,3));
 
 
-const multiply = (a,b) => a*b;
-console.log(multiply(2,2));
+const multiplyNumberOne = (a,b) => a*b;
+console.log(multiplyNumberOne(2,2));
+
 
 const divide = (a,b) => a/b;
 console.log(divide(10,2));
